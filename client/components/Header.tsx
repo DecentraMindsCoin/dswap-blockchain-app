e37ba7358fcd0c9f51b5bfb22d9c923ec0b7dea7
DecentraMindsCoin <decentraminds@gmail.com>
2022-05-21 01:56:14 -0700

    complete UI, just add .env variables for app

--- client/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {useState, useEffect} from 'react'
-
-const Header = () => {
-  return <div>Header</div>
-}
-
-export default Header
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Main from '../components/Main'
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
 }
@@ -13,9 +14,8 @@ const Home: NextPage = () => {
         <title>Dswap Blockchain Application</title>
         <link rel="icon" href="/favicon.ico" />{' '}
       </Head>
-      <h2>Main</h2> 
       <Header />
-      <h2>history</h2>
+      <Main />
     </div>
   )
 }
